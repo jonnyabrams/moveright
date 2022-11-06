c8974407911b29564b0f6a4e9f21557b56c5a1dd
jonnyabrams <jonnyabrams@hotmail.com>
2022-11-06 09:29:02 +0000

    feat: Footer

---
+++ components/Footer.tsx
@@ -0,0 +1,45 @@
+
+
+const Footer = () => {
+  return (
+    <div className="grid grid-cols-1 px-32 text-gray-600 bg-gray-100 gap-y-10 md:grid-cols-4 py-14">
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">Moveright</h5>
+        <p>Search for sale</p>
+        <p>Search for rent</p>
+        <p>Commercial for sale</p>
+        <p>Commercial for rent</p>
+        <p>Search sold prices</p>
+      </div>
+
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">Resources</h5>
+        <p>Where can I live?</p>
+        <p>Stamp duty</p>
+        <p>Students</p>
+        <p>Removals</p>
+        <p>Property guides</p>
+      </div>
+
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">Quick links</h5>
+        <p>Cheap flats to rent</p>
+        <p>Property investment</p>
+        <p>Cheap houses for sale</p>
+        <p>Overseas</p>
+        <p>Find an argument</p>
+      </div>
+
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">Moveright PLC</h5>
+        <p>About</p>
+        <p>Press Centre</p>
+        <p>Investor relations</p>
+        <p>Contact us</p>
+        <p>Careers</p>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Banner from "../components/Banner";
+import Footer from "../components/Footer";
 import Header from "../components/Header";
 import HomeBody from "../components/HomeBody";
 import styles from "../styles/Home.module.css";
@@ -14,8 +15,13 @@ const Home = () => {
       </Head>
 
       <Header />
-      <Banner />
-      <HomeBody />
+      <section>
+        <Banner />
+      </section>
+      <section>
+        <HomeBody />
+      </section>
+      <Footer />
     </div>
   );
 };
